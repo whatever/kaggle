4f5e9f91ab0ad99f24aebf9116cf344e339f9ef6
Matt 3 <not.mattowen@gmail.com>
2019-08-22 23:34:34 -0400

    Comment deriv methods

--- math-aside/deriv.hs
+++ math-aside/deriv.hs
@@ -1,7 +1,11 @@
+-- Define nderiv f ~= f'(x)
 nderiv f  h = \x -> (f(x+h)-f(x-h))/2.0/h
 
+-- f(x) = 3x^2
+f x = 3*x^2
 
-f x = 3*x*x
+-- g(x) ~= f'(x)
 g x = (nderiv f 0.001) x
 
+-- MAIN!
 main = print (g 1)
