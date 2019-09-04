da13995015fe8a453b032697fb9aa3fad096b6f0
Matt 3 <not.mattowen@gmail.com>
2019-09-04 02:41:36 -0400

    Include types for all functions

--- math-aside/multi-descent.hs
+++ math-aside/multi-descent.hs
@@ -1,14 +1,17 @@
 -- Add to vectors
+add :: [Double] -> [Double] -> [Double]
 add xs ys = [x+y | (x, y) <- (zip xs ys)]
 
 
 -- scale
+scale :: [Double] -> Double -> [Double]
 scale xs c = [c*x | x <- xs]
 
 
 -- Return the function that computes the directional derivative for f
 -- NOTE: There's nothing forcing u to be normal
 -- NOTE: I don't know if the / 2.0 is the correct constant here
+directionalDeriv :: ([Double] -> Double) -> [Double] -> [Double] -> Double
 directionalDeriv f u =
   \x ->
     let
@@ -19,12 +22,14 @@ directionalDeriv f u =
 
 
 -- Return the basis vectors for a standard euclidean space
+basis :: Int -> [[Double]]
 basis n = [(dir i n) | i <- [0..(n-1)]]
   where dir i n = [if j == i then 1 else 0 | j <- [0..(n-1)]]
 
 
 -- Return a function that numerically approximates the gradients of a function
 -- NOTE: This just returns a vector the directional derivatives with respect to the standard basis
+grad :: ([Double] -> Double) -> Int -> [Double] -> [Double]
 grad f n =
   \x -> [(g x) | g <- gradients]
   where
@@ -32,10 +37,13 @@ grad f n =
 
 
 -- Return the l2-norm of a vector
+norm :: [Double] -> Double
 norm xs = sqrt (sum [x^2 | x <- xs])
 
 
 -- single iteration of gradient descent
+-- NOTE: This is un-used
+descend :: ([Double] -> Double) -> [Double] -> Int -> [Double]
 descend f x0 n = x1
   where g = (grad f n)
         h = 0.001
@@ -44,6 +52,7 @@ descend f x0 n = x1
 
 -- Probably return a local minimum for a vector value function of n-variables
 -- NOTE: This can be considerably optimized
+gradDescent :: ([Double] -> Double) -> [Double] -> Int -> [Double]
 gradDescent f x0 n = val
   where
     f' = (grad f n)
@@ -55,6 +64,7 @@ gradDescent f x0 n = val
 
 
 -- some multivariate function
+f2 :: [Double] -> Double
 f2 [x, y, z] = (x-3)^2 + y^2 + z^2 + 8
 
 
