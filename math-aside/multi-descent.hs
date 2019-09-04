1dd18c70e34b6cf44b3965771b26488366694361
Matt 3 <not.mattowen@gmail.com>
2019-09-03 20:25:33 -0400

    Rough numerical multivariate gradient descent

--- math-aside/multi-descent.hs
+++ math-aside/multi-descent.hs
@@ -1,5 +1,3 @@
-
-
 -- Add to vectors
 add xs ys = [x+y | (x, y) <- (zip xs ys)]
 
@@ -24,6 +22,9 @@ f [x, y] = (x-3)^2 + (y+2)^2
 fx = nderiv f
 
 
+-- Return the function that computes the directional derivative for f
+-- NOTE: There's nothing forcing u to be normal
+-- NOTE: I don't know if the / 2.0 is the correct constant here
 directionalDeriv f u =
   \x ->
     let
@@ -37,9 +38,45 @@ h = directionalDeriv f [1, 0]
 g = directionalDeriv f [0, 1]
 
 
-f2 [x, y, z] = 2*x + 3*y^2 + 4*z^3
+-- dir
+dir i n = [if j == i then 1 else 0 | j <- [0..(n-1)]]
 
-fg2 = directionalDeriv f2 [0, 0, 1]
+-- Return the basis vectors for a normal hyper plane
+basis n = [(dir i n) | i <- [0..(n-1)]]
+
+
+-- Return a function that numerically approximates the gradients of a function
+grad f n =
+  \x -> [(g x) | g <- gradients]
+  where
+    gradients = [(directionalDeriv f u) | u <- (basis n)]
+
+
+descend f x0 n = x1
+  where g = (grad f n)
+        h = 0.001
+        x1 = (add x0 (scale (g x0) h))
+
+
+
+norm xs = sqrt (sum [x^2 | x <- xs])
+
+gradDescent f x0 n = val
+  where
+    f' = (grad f n)
+    a  = -0.00005
+    x1 = (add x0 (scale (f' x0) a))
+    val = if norm (f' x1) < 0.0001
+             then x1
+             else (gradDescent f x1 n)
+
+
+-- some multivariate function
+f2 [x, y, z] = (x-3)^2 + y^2 + z^2 + 8
+
+
+-- 
+f2' = grad f2 3
 
 
 -- lfg
@@ -47,8 +84,4 @@ main = do
   print "Multivariate Gradient Descent"
   -- print (f [1, 2])
   -- print (f' [1, 2, -3])
-
-  print (fx [3.1, -2.1])
-  print (h [2, 0])
-  print (g [2, 0])
-  print (fg2 [0, 0, 1])
+  print (gradDescent f2 [3, 10, -1] 3)
