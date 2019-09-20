220fef7258e9ec38409369e5dc6d89dc4e3633d5
Matt 3 <not.mattowen@gmail.com>
2019-09-20 16:11:03 -0400

    Neural network converges

--- digital-recognizah/nn.py
+++ digital-recognizah/nn.py
@@ -29,7 +29,7 @@ if __name__ == "__main__":
 
     # Read csv locally
     df = pd.read_csv(args.data)
-    X_features = df.iloc[:, 1:]
+    X_features = df.iloc[:, 1:]/255.0
     Y_labels = pd.get_dummies(df.iloc[:, 0], prefix="digit")
 
     # Helper size variables
@@ -42,36 +42,30 @@ if __name__ == "__main__":
     y = tf.compat.v1.placeholder(tf.float32, [None, num_labels])
 
     # Features -> Hidden
-    W1 = tf.Variable(tf.random.normal([num_features, num_hidden_nodes]), name="W1")
+    W1 = tf.Variable(tf.random.normal([num_features, num_hidden_nodes], stddev=0.01), name="W1")
     b1 = tf.Variable(tf.random.normal([num_hidden_nodes]), name="b1")
 
     # Hidden -> Output
-    W2 = tf.Variable(tf.random.normal([num_hidden_nodes, num_labels]), name="W2")
+    W2 = tf.Variable(tf.random.normal([num_hidden_nodes, num_labels], stddev=0.01), name="W2")
     b2 = tf.Variable(tf.random.normal([num_labels]), name="b2")
 
     # Map from features to hidden layer
-    hidden_out = tf.nn.relu(tf.add(tf.matmul(x, W1), b1))
+    hidden_out = tf.add(tf.matmul(x, W1), b1)
+    hidden_out = tf.nn.relu(hidden_out)
 
     # Map from hidden layer to y
     y_predicted = tf.nn.softmax(tf.add(tf.matmul(hidden_out, W2), b2))
 
     # Let's make predictions look neat
     y_clipped = tf.clip_by_value(y_predicted, 1e-10, 0.9999999)
-
-    # Loss function
-    cross_entropy = -tf.reduce_mean(
-        tf.reduce_sum(
-            y * tf.math.log(y_clipped) + (1 - y) * tf.math.log(1 - y_clipped),
-            axis=1,
-        ),
-    )
-
-    # Initialize the environment
-    init_vars = tf.compat.v1.global_variables_initializer()
+    cross_entropy = -tf.reduce_mean(tf.reduce_sum(y * tf.log(y_clipped) + (1 - y) * tf.log(1 - y_clipped), axis=1))
 
     # Compute % matches
     num_correct = tf.equal(tf.argmax(y, 1), tf.argmax(y_predicted, 1))
     accuracy = tf.reduce_mean(tf.cast(num_correct, tf.float32))
+
+    # Initialize the environment
+    init_vars = tf.compat.v1.global_variables_initializer()
 
     # Hyper parameters
     learning_rate = args.learning_rate
@@ -82,29 +76,29 @@ if __name__ == "__main__":
     optimizer = tf.train.GradientDescentOptimizer(learning_rate=learning_rate)
     train = optimizer.minimize(cross_entropy)
 
-
     # Start the main show
     with tf.Session() as sess:
         sess.run(init_vars)
         total_batches = int(num_examples/batch_size)
         logging.warn("TOTAL BATCHES = %d (siqqq)", total_batches)
 
-        avg_cost = 0.0
+        for _ in range(args.iterations):
+            avg_cost = 0.0
 
-        for i in range(args.iterations):
+            for i in range(total_batches):
 
-            start = batch_size*(i+0)
-            end = batch_size*(i+1)
+                start = batch_size*(i+0)
+                end = batch_size*(i+1)
 
-            batch_x = X_features.iloc[start:end, :]
-            batch_y = Y_labels.iloc[start:end, :]
+                batch_x = X_features.iloc[start:end, :]
+                batch_y = Y_labels.iloc[start:end, :]
 
-            _, cost = sess.run(
-                [train, cross_entropy],
-                feed_dict={x: batch_x, y: batch_y},
-            )
+                _, cost = sess.run(
+                    [train, cross_entropy],
+                    feed_dict={x: batch_x, y: batch_y},
+                )
 
-            avg_cost += cost / total_batches
+                avg_cost += cost / total_batches
 
             logging.warning("Iterations %d: average cost = %f", i, avg_cost)
 
